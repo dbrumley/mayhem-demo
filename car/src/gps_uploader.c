50b9422d778368a2b47ea2fefe0b816a13d12873
David Brumley <dbrumley@forallsecure.com>
2024-08-05 10:31:23 -0400

    mayhem rocks!

--- car/src/gps_uploader.c
+++ car/src/gps_uploader.c
@@ -7,15 +7,15 @@
 
 void divide_by_zero(int lat, int lon) {
   volatile int res = 0;
-  if(lat == 1)
+  if (lat == 1)
     res = lat / lon; // Divide by zero when lon = 0
 }
 
-void integer_overflow_negative(int lat, int lon){
+void integer_overflow_negative(int lat, int lon) {
   // Integer overflow with negative values, difference > INT_MAX
   volatile int res = 0;
 
-  if(lat == 2 && lon == -79927771){
+  if (lat == 2 && lon == -79927771) {
     res = lat - lon + 2067556990;
     printf("Here: %d\n", res);
   }
@@ -55,7 +55,7 @@ void stack_exhaustion(int lat, int lon) {
 
 void vulnerable_c(int bug_num, char* line, int latitude, int longitude) {
   printf("Lat: %d Lon: %d\n", latitude, longitude);
-  if(bug_num == 0){
+  if (bug_num == 0) {
     divide_by_zero(latitude, longitude);
     integer_overflow_negative(latitude, longitude);
     oob_read(latitude, longitude);
@@ -64,12 +64,12 @@ void vulnerable_c(int bug_num, char* line, int latitude, int longitude) {
     stack_exhaustion(latitude, longitude);
   }
 
-  if(bug_num == 1) integer_overflow_negative(latitude, longitude);
-  if(bug_num == 2) divide_by_zero(latitude, longitude);
-  if(bug_num == 3) oob_read(latitude, longitude);
-  if(bug_num == 4) oob_write(latitude, longitude);
-  if(bug_num == 5) double_free(latitude, longitude);
-  if(bug_num == 6) stack_exhaustion(latitude, longitude);
+  if (bug_num == 1) integer_overflow_negative(latitude, longitude);
+  if (bug_num == 2) divide_by_zero(latitude, longitude);
+  if (bug_num == 3) oob_read(latitude, longitude);
+  if (bug_num == 4) oob_write(latitude, longitude);
+  if (bug_num == 5) double_free(latitude, longitude);
+  if (bug_num == 6) stack_exhaustion(latitude, longitude);
 }
 
 
@@ -80,7 +80,7 @@ int parse_lat_lon(char* line, int* latitude, int* longitude) {
   int field_count = 0;
   int lat = 0, lon = 0;
 
-  str = line; 
+  str = line;
 
   fields[field_count++] = str;
   for (char* ptr = str; *ptr != '\0' && field_count < 15; ptr++) {
@@ -104,33 +104,51 @@ int parse_lat_lon(char* line, int* latitude, int* longitude) {
   *longitude = lon;
 
   return 0;
-} 
+}
 
+// Function to upload position data to API server
+void upload_position(const char* URL, double latitude, double longitude)
+{
+  char command[256];
+  if (URL == NULL) {
+    printf("No server to upload latitude: %f, longitude: %f\n", latitude, longitude);
+    return;
+  }
+
+  printf("Uploading lat %f lon %f to %s\n", latitude, longitude, URL);
+  snprintf(command, sizeof(command),
+    "curl -X POST -H \"Content-Type: application/json\"  -u me@me.com:123456 -d '{\"latitude\": %.6f, \"longitude\": %.6f}' %s",
+    latitude, longitude, URL);
+
+  system(command);
+}
 
 int main(int argc, char* argv[])
 {
   char line[256];
   int lat = 0, lon = 0;
-  double latitude=0.0, longitude=0.0; 
+  double latitude = 0.0, longitude = 0.0;
   const char* GPS_FILE_PATH = NULL;
   int fd, in_bytes;
 
   GPS_FILE_PATH = argv[1];
 
-  if( (fd = open(GPS_FILE_PATH, O_RDONLY)) == -1)
-    exit(-1); 
+  if ((fd = open(GPS_FILE_PATH, O_RDONLY)) == -1)
+    exit(-1);
 
-  if( (in_bytes = read(fd, line, sizeof(line)-1)) == -1)
+  if ((in_bytes = read(fd, line, sizeof(line) - 1)) == -1)
     exit(-1);
 
   line[in_bytes] = 0;
   parse_lat_lon(line, &lat, &lon);
   vulnerable_c(0, line, lat, lon);
 
-  latitude = (double) lat/ 1000000;
-  longitude = (double) lon / 1000000;
+  latitude = (double)lat / 1000000;
+  longitude = (double)lon / 1000000;
 
-  //printf("Here: %lf %lf\n", latitude, longitude);
+  if (argc == 3) {
+    upload_position(argv[2], (double)lat / (double)1000000, (double)lon / (double)1000000);
+  }
   return 0;
 }
 
